48b8402d97d552780f1c70889b4f166faa4e1ec9
David Legendre <legendre2013@hotmail.com>
2023-08-24 15:51:52 -0500

    adding new docker file

--- MailServices/Program.cs
+++ MailServices/Program.cs
@@ -3,7 +3,14 @@ using MailServices.Helpers;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+//public static IHostBuilder CreateHostBuilder(string[] args) =>
+//    Host.CreateDefaultBuilder(args)
+//        .ConfigureWebHostDefaults(webBuilder =>
+//        {
+//            webBuilder.UseStartup<Startup>();
+//            var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+//            webBuilder.UseUrls($"http://*:{port}/");
+//        });
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
